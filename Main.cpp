023a0bad2d076e8015dc97aa03e45158ade9b813
Axine <110091391+axine0@users.noreply.github.com>
2022-07-31 01:49:27 -0400

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -12,8 +12,9 @@ int main()
 	if (processName.find(".exe") == std::string::npos)
 		processName += ".exe";
 
-	if (Process::GetHandle(processName.c_str()))
+	if (HANDLE hHandle = Process::GetHandle(processName.c_str()))
 	{
+		CloseHandle(hHandle);
 		system("cls");
 		Query::NativeAddresses(processName);
 		Query::MemoryRegions(processName);
@@ -21,7 +22,7 @@ int main()
 	else
 	{
 		system("cls");
-		std::cout << "Unable to parse process\n" << "Closing in 2 seconds\n";
+		std::cout << "Unable to parse process\nClosing in 2 seconds\n";
 		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 		exit(0);
 	}
